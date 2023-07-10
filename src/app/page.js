c3f4c2e495f973e24796405029d97b58dd168308
Ryan Beitler <rybeitler@gmail.com>
2023-07-10 19:10:18 -0400

    fin?

--- src/app/page.js
+++ src/app/page.js
@@ -23,7 +23,7 @@ export default function Home() {
         <div>
           {
             !skills?
-                  <p className="text-3xl"><span className='italic'>"Simplicity is the ultimate sophistication."</span> - Leonardo da Vinci</p>
+                  <p className="text-3xl"><span className='italic'>Simplicity is the ultimate sophistication.</span> - Leonardo da Vinci</p>
                   :
                   <div className='flex flex-wrap'>
                     <a className='h-30 w-30' href="https://docs.microsoft.com/en-us/dotnet/csharp/" target="_blank" rel="noreferrer"><img className='w-[8rem] h-[8rem]' src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/csharp-colored.svg" width="36" height="36" alt="C#" /></a>
